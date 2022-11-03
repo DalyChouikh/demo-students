bf600a5f019c3cbabba724129ef6ee2e0b8e0166
DalyChouikh <chouikhdaly215@gmail.com>
2022-11-03 21:40:45 +0100

    Added PUT request

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,7 +1,10 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,5 +40,25 @@ public class StudentService {
 		}
 		studentRepository.deleteById(studentId);
 	}
-    
+	@Transactional
+	public void updateStudent(Long studentId, String name, String email){
+		Student student = studentRepository.findById(studentId)
+		.orElseThrow(() -> new IllegalStateException("Student with ID : " + studentId + " does not exists"));
+
+		if(!(name==null) &&
+		name.length() > 0 &&
+		!Objects.equals(name, student.getName())){
+		student.setName(name);
+		}
+		if(!(email==null) &&
+		email.length() > 0 &&
+		!Objects.equals(email, student.getEmail())){
+		Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+		if(studentOptional.isPresent()){
+			throw new IllegalStateException("Email is already taken");
+		}
+		student.setEmail(email);
+		}
+
+	}
 }
